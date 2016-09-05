821da0898f770feea97be2a3819f4e6ca6d9ca3e
xermicus <bigcyrill@hotmail.com>
2016-09-05 22:35:34 +0000

    geocoding

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var googleMapsClient = require('@google/maps').createClient({
+	key: ''
+});
 
 // redis stuff
 var redis = require("redis")
 var r= redis.createClient(6379,'10.10.0.107');
 r.auth('letmeinplease');
 r.on("error", function (err) {
-    console.log("Redis Error " + err);
+    console.log("Redis Error: " + err);
 });
-r.set("string key", "string val", redis.print);
 
 // Basic routing
 app.get('/', function(req, res){
@@ -31,7 +33,35 @@ io.on('connection', function(socket) {
 
         // send data
         var interval = setInterval(function () {
-                socket.emit('get', 'hi');
+                r.setnx('keyword', 'unset');
+		pk = 0
+		txt = ''
+		loc = ''
+		r.spop(['done'], function(err, reply) { 
+			pk = reply;
+			console.log(reply);
+			r.hgetall(pk.toString(), function(err, reply) {
+				if(err) { console.log(err) } else {
+					txt = reply.text;
+					loc = reply.location;
+					rep = reply.class;
+					console.log(txt);
+					//console.log(loc);
+					console.log(rep);
+					googleMapsClient.geocode({
+					address: loc
+					}, function(err, response) {
+						if (!err) {
+							console.log(response.json.results);
+						}
+					});
+				}
+			});
+		});
+		
+		//loc = r.hget(pk, 'location', redis.print);
+		//socket.emit('get', 'hi');
+
         }, 3000);
 }); 
     
